19270dbecdbb45751515f6e9ac429dd8d704487d
viniciuszago <mvinicius.zago@gmail.com>
2014-07-28 15:55:27 -0300

    Fix create partial in create folder

--- cli/lib/commands/create_partial.js
+++ cli/lib/commands/create_partial.js
@@ -7,28 +7,29 @@
 
   CreatePartial = (function() {
     function CreatePartial() {
-      var data, file_name, folder_name, name, values;
+      var file_name, folder_name, name, values;
       values = program.args[0].split("/");
       folder_name = Utils.pluralize(values[0]);
       name = Utils.lowercase(values[1]);
-      file_name = "" + folder_name + "/" + name + ".jade";
-      fs.exists(path.join(partials_folder, file_name), function(exists) {
+      file_name = "" + name + ".jade";
+      fs.exists(path.join(partials_folder, folder_name, file_name), function(exists) {
         if (exists) {
           console.log("File already exists.");
           return process.exit(0);
         }
       });
       fs.exists(path.join(partials_folder, folder_name), function(exists) {
+        var data;
         if (!exists) {
-          return fs.mkdir(path.join(partials_folder, folder_name), '755');
+          fs.mkdir(path.join(partials_folder, folder_name), '755');
         }
-      });
-      data = "p " + name;
-      fs.writeFile(path.join(partials_folder, file_name), data, 'utf8', function(err) {
-        if (err) {
-          throw err;
-        }
-        return console.log("File created successfully.");
+        data = "p " + name;
+        return fs.writeFile(path.join(partials_folder, folder_name, file_name), data, 'utf8', function(err) {
+          if (err) {
+            throw err;
+          }
+          return console.log("File created successfully.");
+        });
       });
     }
 
--- cli/src/commands/create_partial.coffee
+++ cli/src/commands/create_partial.coffee
@@ -8,9 +8,9 @@ class CreatePartial
 		values = program.args[0].split("/")
 		folder_name = Utils.pluralize(values[0])
 		name = Utils.lowercase(values[1])
-		file_name = "#{folder_name}/#{name}.jade"
+		file_name = "#{name}.jade"
 
-		fs.exists path.join( partials_folder, file_name), (exists) ->
+		fs.exists path.join( partials_folder, folder_name, file_name), (exists) ->
 			if exists
 				console.log("File already exists.")
 				process.exit(0)
@@ -19,10 +19,9 @@ class CreatePartial
 			if !exists
 				fs.mkdir path.join(partials_folder, folder_name), '755'
 
-		data = "p #{name}"
-		
-		fs.writeFile path.join( partials_folder, file_name), data, 'utf8', (err) ->
-			throw err if err
-			console.log "File created successfully."
+			data = "p #{name}"
+			fs.writeFile path.join( partials_folder, folder_name, file_name), data, 'utf8', (err) ->
+				throw err if err
+				console.log "File created successfully."
 
 module.exports =  CreatePartial
