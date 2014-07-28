9bd42eb4be24e82b20f6eb92e41b334c5439b47c
Marcelo Eduardo de Andrade Jr <marcelinhov2@Marcelos-MacBook-Pro.local>
2014-07-28 13:08:38 -0300

    writing and stylizing the helper

--- cli/lib/angular-kickoff.js
+++ cli/lib/angular-kickoff.js
@@ -16,10 +16,30 @@
   AngularKickoff = (function() {
     function AngularKickoff() {
       program.on('--help', function() {
+        console.log('  Ng-Classify Modules Available (module_name): ');
+        console.log('');
+        console.log('    animation / config / constant / controller / directive /'.cyan);
+        console.log('    factory / filter / provider / run / service / value'.cyan);
+        console.log('');
         console.log('  Examples:');
         console.log('');
-        console.log('    To create modules use:');
-        console.log('    	$ angular-kickoff -g module folder/module');
+        console.log('    To create modules:');
+        console.log('      $ angular-kickoff -g module module_name/class_name'.green);
+        console.log('');
+        console.log('    To create partials:');
+        console.log('      $ angular-kickoff -g partial folder_name/name'.green);
+        console.log('');
+        console.log('    To create styles:');
+        console.log('      $ angular-kickoff -g style name'.green);
+        console.log('');
+        console.log('    To destroy modules:');
+        console.log('      $ angular-kickoff -d module module_name/class_name'.red);
+        console.log('');
+        console.log('    To destroy partials:');
+        console.log('      $ angular-kickoff -d partial folder_name/name'.red);
+        console.log('');
+        console.log('    To destroy styles:');
+        console.log('      $ angular-kickoff -d style name'.red);
         return console.log('');
       });
       program.version('0.1.0').option('-g, --generate <option> [value]', 'Generate files').option('-d, --destroy <option> [value]', 'Generate files').parse(process.argv);
--- cli/src/angular-kickoff.coffee
+++ cli/src/angular-kickoff.coffee
@@ -11,12 +11,32 @@ DestroyStyle = require './commands/destroy_style'
 class AngularKickoff
 	constructor:() ->
 		program.on '--help', ->
-		  console.log '  Examples:'
-		  console.log ''
-		  console.log '    To create modules use:'
-		  console.log '    	$ angular-kickoff -g module folder/module'
-		  console.log ''
-		  
+      console.log '  Ng-Classify Modules Available (module_name): '
+      console.log '' 
+      console.log '    animation / config / constant / controller / directive /'.cyan
+      console.log '    factory / filter / provider / run / service / value'.cyan
+      console.log ''
+      console.log '  Examples:'
+      console.log '' 
+      console.log '    To create modules:'
+      console.log '      $ angular-kickoff -g module module_name/class_name'.green
+      console.log '' 
+      console.log '    To create partials:'
+      console.log '      $ angular-kickoff -g partial folder_name/name'.green
+      console.log ''
+      console.log '    To create styles:'
+      console.log '      $ angular-kickoff -g style name'.green
+      console.log ''
+      console.log '    To destroy modules:'
+      console.log '      $ angular-kickoff -d module module_name/class_name'.red
+      console.log '' 
+      console.log '    To destroy partials:'
+      console.log '      $ angular-kickoff -d partial folder_name/name'.red
+      console.log ''
+      console.log '    To destroy styles:'
+      console.log '      $ angular-kickoff -d style name'.red
+      console.log ''
+
 		program
 			.version('0.1.0')
 			.option('-g, --generate <option> [value]', 'Generate files')
